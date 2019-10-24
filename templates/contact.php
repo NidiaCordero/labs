f9772b1f3607b0dca8276319368b4c0e97f95da7
Cordero Nidia <ruizcorderonidia@gmail.com>
2019-10-24 15:33:42 +0200

    add costum field to section contact

--- templates/contact.php
+++ templates/contact.php
@@ -1,38 +1,77 @@
+<?php
 
-	<!-- Contact section -->
-	<div class="contact-section spad fix">
-		<div class="container">
-			<div class="row">
-				<!-- contact info -->
-				<div class="col-md-5 col-md-offset-1 contact-info col-push">
-					<div class="section-title left">
-						<h2>Contact us</h2>
-					</div>
-					<p>Cras ex mauris, ornare eget pretium sit amet, dignissim et turpis. Nunc nec maximus dui, vel suscipit dolor. Donec elementum velit a orci facilisis rutrum. </p>
-					<h3 class="mt60">Main Office</h3>
-					<p class="con-item">C/ Libertad, 34 <br> 05200 Arévalo </p>
-					<p class="con-item">0034 37483 2445 322</p>
-					<p class="con-item">hello@company.com</p>
-				</div>
-				<!-- contact form -->
-				<div class="col-md-6 col-pull">
-					<form class="form-class" id="con_form">
-						<div class="row">
-							<div class="col-sm-6">
-								<input type="text" name="name" placeholder="Your name">
-							</div>
-							<div class="col-sm-6">
-								<input type="text" name="email" placeholder="Your email">
-							</div>
-							<div class="col-sm-12">
-								<input type="text" name="subject" placeholder="Subject">
-								<textarea name="message" placeholder="Message"></textarea>
-								<button class="site-btn">send</button>
-							</div>
+$args = array(
+	'post_type' => 'post',
+	'post_status' => 'publish',
+	'category_name' => 'contact',
+	'posts_per_page' => 1,
+);
+$arr_posts = new WP_Query($args);
+
+if ($arr_posts->have_posts()) :
+
+	while ($arr_posts->have_posts()) :
+		$arr_posts->the_post();
+		?>
+		<!-- Contact section -->
+		<div class="contact-section spad fix">
+			<div class="container">
+				<div class="row">
+					<!-- contact info -->
+					<div class="col-md-5 col-md-offset-1 contact-info col-push">
+						<div class="section-title left">
+							<?php
+									$before_title = get_field('titre_before');
+									$span_title = get_field('span_title');
+									$titre_after = get_field('titre_after');
+
+
+
+									if (get_field('titre_before')) {
+										echo '<h2>' . $before_title . '&nbsp <span>' . $span_title . '  </span>' . '&nbsp' . $titre_after . '</h2>';
+									} else {
+										?> <h2> <?php the_title(); ?> </h2>
+							<?php
+									}
+									?>
 						</div>
-					</form>
+						<p><?php echo excerpt(40); ?></p>
+						<h3 class="mt60"><?php the_field('name_office'); ?></h3>
+						<p class="con-item"><?php the_field('adress_1'); ?><br> <?php the_field('adress_2'); ?> </p>
+						<p class="con-item"><?php the_field('phone'); ?></p>
+						<p class="con-item"><?php the_field('mail_perso'); ?></p>
+					</div>
+					<!-- contact form -->
+					<div class="col-md-6 col-pull">
+						<form class="form-class" id="con_form">
+							<div class="row">
+								<div class="col-sm-6">
+									<input type="text" name="name" placeholder="<?php the_field('placeholder_name'); ?>">
+								</div>
+								<div class="col-sm-6">
+									<input type="text" name="email" placeholder="<?php the_field('placeholder_mail'); ?>">
+								</div>
+								<div class="col-sm-12">
+									<input type="text" name="subject" placeholder="<?php the_field('placeholder_name'); ?>">
+									<textarea name="message" placeholder="<?php the_field('placeholder_subject'); ?>"></textarea>
+
+									<?php
+											$btn_name = get_field('btn_read_more');
+
+											if (get_field('btn_read_more')) {
+												echo '<button class="site-btn">' . $btn_name . '</button>';
+											} else {
+												echo ' ';
+											}
+											?>
+								</div>
+							</div>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<!-- Contact section end-->
+<?php
+	endwhile;
+endif; ?>
+<!-- Contact section end-->
--- templates/promotion.php
+++ templates/promotion.php
@@ -1,6 +1,5 @@
-
-	<!-- Promotion section -->
-	<?php
+<!-- Promotion section -->
+<?php
 
 $args = array(
 	'post_type' => 'post',
@@ -15,44 +14,44 @@ if ($arr_posts->have_posts()) :
 	while ($arr_posts->have_posts()) :
 		$arr_posts->the_post();
 		?>
-	<div class="promotion-section">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-9">
-				<?php
-							$before_title = get_field('titre_before');
-							$span_title = get_field('span_title');
-							$titre_after = get_field('titre_after');
-
-
-
-							if (get_field('titre_before')) {
-								echo '<h2>' . $before_title . '&nbsp <span>' . $span_title . '  </span>' . '&nbsp' . $titre_after . '</h2>';
-							} else {
-								?> <h2> <?php the_title(); ?> </h2>
-					<?php
-							}
-							?>
-					<p><?php echo excerpt(25); ?></p>
-				</div>
-				<div class="col-md-3">
-					<div class="promo-btn-area">
-					
+		<div class="promotion-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-9">
 						<?php
-							$btn_name = get_field('btn_read_more');
+								$before_title = get_field('titre_before');
+								$span_title = get_field('span_title');
+								$titre_after = get_field('titre_after');
 
-							if (get_field('btn_read_more')) {
-								echo '<a href="" class="site-btn btn-2">' . $btn_name . '</a>';
-							} else {
-								echo ' ';
-							}
-							?>
+
+
+								if (get_field('titre_before')) {
+									echo '<h2>' . $before_title . '&nbsp <span>' . $span_title . '  </span>' . '&nbsp' . $titre_after . '</h2>';
+								} else {
+									?> <h2> <?php the_title(); ?> </h2>
+						<?php
+								}
+								?>
+						<p><?php echo excerpt(25); ?></p>
+					</div>
+					<div class="col-md-3">
+						<div class="promo-btn-area">
+
+							<?php
+									$btn_name = get_field('btn_read_more');
+
+									if (get_field('btn_read_more')) {
+										echo '<a href="" class="site-btn btn-2">' . $btn_name . '</a>';
+									} else {
+										echo ' ';
+									}
+									?>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<?php
-				endwhile;
-			endif; ?>
-	<!-- Promotion section end-->
+<?php
+	endwhile;
+endif; ?>
+<!-- Promotion section end-->
