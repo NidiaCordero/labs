f58bd36682d0acda5d7ee0672a27a70de50ae314
Cordero Nidia <ruizcorderonidia@gmail.com>
2019-10-28 11:29:29 +0100

    add search page and function

--- search.php
+++ search.php
@@ -1,30 +1,78 @@
+<?php /* Template Name: search page */ ?>
+
 <?php
-    global $query_string;
-    $query_args = explode("&", $query_string);
-    $search_query = array();
+get_template_part('templates/header-page');
+?>
 
-    foreach($query_args as $key => $string) {
-      $query_split = explode("=", $string);
-      $search_query[$query_split[0]] = urldecode($query_split[1]);
-    } // foreach
+<div class="page-section spad">
+    <div class="container">
+        <div class="row">
+            <?php
+            global $query_string;
+            $query_args = explode("&", $query_string);
+            $search_query = array();
 
-    $the_query = new WP_Query($search_query);
-    if ( $the_query->have_posts() ) : 
-    ?>
-    <!-- the loop -->
+            foreach ($query_args as $key => $string) {
+                $query_split = explode("=", $string);
+                $search_query[$query_split[0]] = urldecode($query_split[1]);
+            } // foreach
 
-    <ul>    
-    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-        <li>
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-        </li>   
-        <?php the_content() ?>
-    <?php endwhile; ?>
-    </ul>
-    <!-- end of the loop -->
+            $the_query = new WP_Query($search_query);
+            if ($the_query->have_posts()) :
+                ?>
+                <!-- the loop -->
+                <div class="col-md-8 col-sm-7 blog-posts">
 
-    <?php wp_reset_postdata(); ?>
+                    <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                        <div class="post-item">
+                            <div class="post-thumbnail">
+                                <?php
+                                        $featured_img_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+                                        echo '<img src="' . $featured_img_url . '" alt="">';
+                                        ?>
+                                <div class="post-date">
+
+                                    <h2><?php echo get_the_date('d'); ?></h2>
+                                    <h3><?php echo get_the_date('M Y'); ?></h3>
+                                </div>
+
+                            </div>
+                            <div class="post-content">
+                                <h2 class="post-title"><?php the_title(); ?></h2>
+                                <div class="post-meta">
+
+                                    <?php
+                                            $tags = get_tags();
+                                            if ($tags) :
+                                                foreach ($tags as $tag) : ?>
+                                            <a href="<?php echo esc_url(get_tag_link($tag->term_id)); ?>" title="<?php echo esc_attr($tag->name); ?>"><?php echo esc_html($tag->name); ?></a>
+                                        <?php endforeach; ?>
+                                    <?php endif; ?>
+
+                                    <!-- Post item -->
+                                </div>
+                                <p><?php echo excerpt_blog(30); ?></p>
+                            </div>
+                        </div>
+                    <?php endwhile; ?>
+
+                </div>
+                <?php get_template_part('templates-blog/sidebar-blog'); ?>
+
+        </div>
+
+        <!-- end -->
+        <?php wp_reset_postdata(); ?>
+    </div>
+</div>
+
+<!-- end of the loop -->
+
 
 <?php else : ?>
-    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+    <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
 <?php endif; ?>
+<?php
+get_footer();
+
+?>
--- templates-blog/last-blog.php
+++ templates-blog/last-blog.php
@@ -80,64 +80,7 @@
                 </div>
             </div>
             <!-- Sidebar area -->
-            <div class="col-md-4 col-sm-5 sidebar">
-                <!-- Single widget -->
-                <div class="widget-item">
-                    <form action="#" class="search-form">
-                        <input type="text" placeholder="Search">
-                        <button class="search-btn"><i class="flaticon-026-search"></i></button>
-                    </form>
-                </div>
-                <!-- Single widget -->
-                <div class="widget-item">
-                    <!-- <h2 class="widget-title">Categories</h2>
-                    <ul> -->
-                    <?php dynamic_sidebar( 'custom-side-bar' ); ?>
-                      
-                    <!-- </ul> -->
- 
-                </div>
-                <!-- Single widget -->
-                <div class="widget-item">
-                    <h2 class="widget-title">Instagram</h2>
-                    <ul class="instagram">
-                        <li><img src="img/instagram/1.jpg" alt=""></li>
-                        <li><img src="img/instagram/2.jpg" alt=""></li>
-                        <li><img src="img/instagram/3.jpg" alt=""></li>
-                        <li><img src="img/instagram/4.jpg" alt=""></li>
-                        <li><img src="img/instagram/5.jpg" alt=""></li>
-                        <li><img src="img/instagram/6.jpg" alt=""></li>
-                    </ul>
-                </div>
-                <!-- Single widget -->
-                <div class="widget-item">
-                    <h2 class="widget-title">Tags</h2>
-                    <ul class="tag">
-                        <li><a href="">branding</a></li>
-                        <li><a href="">identity</a></li>
-                        <li><a href="">video</a></li>
-                        <li><a href="">design</a></li>
-                        <li><a href="">inspiration</a></li>
-                        <li><a href="">web design</a></li>
-                        <li><a href="">photography</a></li>
-                    </ul>
-                </div>
-                <!-- Single widget -->
-                <div class="widget-item">
-                    <h2 class="widget-title">Quote</h2>
-                    <div class="quote">
-                        <span class="quotation">‘​‌‘​‌</span>
-                        <p>Vivamus in urna eu enim porttitor consequat. Proin vitae pulvinar libero. Proin ut hendrerit metus. Aliquam erat volutpat. Donec fermen tum convallis ante eget tristique. Sed lacinia turpis at ultricies vestibulum.</p>
-                    </div>
-                </div>
-                <!-- Single widget -->
-                <div class="widget-item">
-                    <h2 class="widget-title">Add</h2>
-                    <div class="add">
-                        <a href=""><img src="img/add.jpg" alt=""></a>
-                    </div>
-                </div>
-            </div>
+            <?php get_template_part('templates-blog/sidebar-blog'); ?>
         </div>
     </div>
 </div>
